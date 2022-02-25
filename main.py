e7c3e62f396bf1ec83a773e46c762fa7544e058f
Ramesh SS KHARAT <ramesh@priarworld.com>
2022-02-25 17:05:25 +0530

    fix:v1.0.0

---
+++ main.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+def main():
+  # echo output
+  for i in range(100):
+      print("XXX" * i)
+      print(i)
+      print ("XXX" * i)
+if __name__ == '__main__':
+  main()
